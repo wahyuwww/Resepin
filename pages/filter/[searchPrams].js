469aa8c299691dd2c67328cca0d5253090de797d
wahyuwww <wahyu.aan2508@gmail.com>
2022-06-23 14:38:17 +0700

    deploy next js

---
+++ pages/filter/[searchPrams].js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../../components/base/footer/footer';
+import Navbars from '../../components/base/navbar/navbar';
+
+const Search = () => {
+  return (
+      <div>
+          <Navbars></Navbars>
+          <Footer></Footer>
+    </div>
+  )
+}
+
+export default Search;
