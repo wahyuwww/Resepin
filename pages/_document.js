02fb9ee77708a0a21011d96afee456e35709fce9
wahyuwww <wahyu.aan2508@gmail.com>
2022-06-23 14:33:07 +0700

    deploy

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
-       <Link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <Link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-        <Link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet"></Link>
+       <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet"></link>
       </Head>
       <body>
         <Main />
