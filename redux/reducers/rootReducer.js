e09293ccf33d29091b0f0a3b630e1b6788092b26
wahyuwww <wahyu.aan2508@gmail.com>
2022-07-26 07:33:54 +0700

    make it better

--- redux/reducers/rootReducer.js
+++ redux/reducers/rootReducer.js
@@ -1,7 +1,7 @@
 import {combineReducers} from 'redux'
 import {userReducer} from './userReducer'
 const rootReducer = combineReducers({
-  auth: userReducer,
+  auths: userReducer,
 });
 
 export default rootReducer
